f1072cb963392fa7abba7c31027bd639b2977d21
25A31A05KH <bobbilirayudu264@gmail.com>
2026-01-28 15:41:01 +0530

    Add files via upload
    DS Basic Problems week 1

---
+++ Rayudu.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"NAME : rayudu\n";
+	cout<<"ROLL NO : 25A31A05KH\n";
+	cout<<"SECTION : F\n";
+	cout<<"BRANCH : CSE\n";
+	return 0;
+}
